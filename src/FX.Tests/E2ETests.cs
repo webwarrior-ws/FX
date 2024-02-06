b7c6c9248fc5e38ef79a9e9875f07d7f5a0b5cb0
webwarrior <reg@webwarrior.ws>
2024-02-06 13:29:46 +0100

    WIP: make tests run on Linux

--- src/FX.Tests/E2ETests.cs
+++ src/FX.Tests/E2ETests.cs
@@ -13,25 +13,47 @@ namespace FsharpExchange.Tests
     [TestFixture]
     public class E2ETests
     {
+        private Process LaunchGrpcServer()
+        {
+            var solutionDir = (new DirectoryInfo(Environment.CurrentDirectory)).Parent?.Parent?.Parent?.Parent?.Parent;
+            var serviceExeDir = Path.Join(solutionDir.FullName, "src", "FX.GrpcService", "bin", "Debug", "net8.0");
+
+            var argsString = $"--urls https://localhost:{GrpcClient.Instance.Port}";
+
+            if (OperatingSystem.IsWindows())
+            {
+                var serviceExePath = Path.Join(serviceExeDir, "FX.GrpcService.exe");
+                return Process.Start(serviceExePath, argsString);
+            }
+            else 
+            {
+                var processInfo = new ProcessStartInfo
+                {
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    FileName = "dotnet",
+                    Arguments = Path.Join(serviceExeDir, "FX.GrpcService.dll") + " " + argsString
+                };
+                return Process.Start(processInfo);
+            }
+        }
+
         [Test]
         async public Task GRPCE2ETest()
         {
             var solutionDir = (new DirectoryInfo(Environment.CurrentDirectory)).Parent?.Parent?.Parent?.Parent?.Parent;
-            var serviceExePath =
-                Path.Join(solutionDir.FullName, "src", "FX.GrpcService", "bin", "Debug", "net8.0", "FX.GrpcService.exe");
+            var serviceExeDir = Path.Join(solutionDir.FullName, "src", "FX.GrpcService", "bin", "Debug", "net8.0");
 
-            using (var serverProcess = Process.Start(serviceExePath, $"--urls https://localhost:{GrpcClient.Instance.Port}"))
-            {
-                await Task.Delay(1000);
+            using var serverProcess = LaunchGrpcServer();
+            await Task.Delay(1000);
 
-                var client = new GrpcClient.Instance();
-                client.Connect();
+            var client = new GrpcClient.Instance();
+            client.Connect();
 
-                var message = "hello";
-                var response = await client.SendMessage(message);
+            var message = "hello";
+            var response = await client.SendMessage(message);
 
-                Assert.AreEqual("received " + message, response);
-            }
+            Assert.AreEqual("received " + message, response);
         }
     }
 }
